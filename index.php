50eeb08cf32d8a62c37a2b6d1442ae74467c1e58
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-21 22:48:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang='th'> 
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hovercraft</title>
+    <link rel="stylesheet" href="style.css">
+
+  
+
+</head>
+<body>
+
+    <div class="menubar">
+        <div class="container">
+            <div class="logo">
+                <h1>Hovercraft Group</h1>
+            </div>
+            <ul class="menu">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Ball</a>
+                </li>
+                <li>
+                    <a href="bee.html">Bee</a>
+                </li>
+                <li>
+                    <a href="jib.html">Jib</a>
+                </li>
+                <li>
+                    <a href="https://phazanicwebtech.herokuapp.com/">Phai</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <header class="header">
+        <div class="container">
+            <div class="header_area">
+                <h1>Welcome to <br>Hovercraft Group</h1>
+                <p>DS-WebTechnology 63</p>
+            </div>
+        </div>
+    </header>
+
+    <section class="info1">
+        <div class="container">
+            <div class="info1_area">
+                <img src="p11.jpg" alt="">
+                <div class="info1_text">
+                    <h1>Jiraphat Thongyoo</h1>
+                    <p>620710040<br>Data Science<br>SU76 SC48 DS2</p>
+                    <a href="https://beewebtech.herokuapp.com/"><button>...Click...</button><br></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="info2">
+        <div class="container">
+            <div class="info2_area">
+                <div class="info2_text">
+                    <h1>Phanthipha Makchiao</h1>
+                    <p>620710161<br>Data Science<br>SU76 SC48 DS2</p>
+             
+                    <a href="bee.html"><button>...Click...</button><br></a>
+                </div>
+            
+                <img src="p22.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="info3">
+        <div class="container">
+            <div class="info3_area">
+                <div class="info3_text">
+                    <h1>Apatsara Piromto</h1>
+                    <p>620710162<br>Data Science<br>SU76 SC48 DS2</p>
+                    <a href="jib.html"><button>...Click...</button><br></a>
+                </div>
+                <img src="p33.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="info4">
+        <div class="container">
+            <div class="info4_area">
+                <div class="info4_text">
+                    <h1>Worapon Boontawee</h1>
+                    <p>620710742<br>Data Science<br>SU76 SC48 DS2</p>
+                    <a href="https://phazanicwebtech.herokuapp.com/"><button>...Click...</button><br></a>
+                </div>
+                <img src="p44.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <div class="clearfix"></div>
+
+
+
+
+
+
+
+
+    <footer>
+        <p>Copyright &copy; 2020 | Hovercraft</p>
+    </footer>
+    
+</body>
+</html>
