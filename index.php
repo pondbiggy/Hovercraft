0766dfe457d8288cfd6dbea3bd8e525c4668613f
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-29 10:20:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,127 +1,47 @@
-<!doctype html>
-<html lang="th">
+<!DOCTYPE html>
+<html lang='th'> 
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hovercraft</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <link rel="stylesheet" href="style.css">
-    <style>
-  /* Make the image fully responsive */
-  .carousel-inner img {
-    width: 50%;
-    height: 50%;
-  }
+    
+
   
-    body{
-           text-align: center ;
-           
-        }
-    .card {
-             margin: auto;
-            }
-    .btn btn-primary{
-             margin: auto;
-             text-align: center ;
-            }
-    .carousel slide{
-             margin: auto;
-             text-align: center ;
-
-            }
-    .Section Contact lazy-bg {
-             text-align:center;
-             margin: auto;
-             margin-top: 200px;
-    }
-    #onfoot {
-                background-color:#c3d5d5; 
-
-            }
-        
-
-  </style>
-
-
 
 </head>
-
-
-
-
 <body>
 
-
-<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-   <!-- Brand/logo -->
-   <a class="navbar-brand" href="#">
-    <img src="ho.png" alt="logo" style="width:40px;">
-  </a>
-  <!-- Brand -->
-  <a class="navbar-brand" href="https://hovercraft4.herokuapp.com/"><strong>Hovercraft</strong></a>
-
-  <!-- Toggler/collapsibe Button 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button> -->
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#contact">Contact</a>
-      </li>
-      <!-- Dropdown -->
-      <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">About</a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="https://www.su.ac.th/th/index.php">Silpakorn University</a>
-        <a class="dropdown-item" href="http://www.sc.su.ac.th/index.php">Science, Silpakorn</a>
-        <a class="dropdown-item" href="https://www.cp.su.ac.th/">Department of Computing</a>
-        <a class="dropdown-item" href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">DataScience</a>
-      </div>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Members
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="ball1.html">Ball</a>
-        <a class="dropdown-item" href="b.html">Bee</a>
-        <a class="dropdown-item" href="jib.html">Jib</a>
-        <a class="dropdown-item" href="phaii.html">Phai</a>
-      </div>
-    </li>
-    </ul>
-    
-  <div class="pos-f-t">
-    <div class="collapse" id="navbarToggleExternalContent">
-      <div class="bg-dark p-4">
-        <h4 class="text-white">Collapsed content</h4>
-        <span class="text-muted">Toggleable via the navbar brand.</span>
-      </div>
+    <div class="menubar">
+        <div class="container">
+            <div class="logo">
+              <h1>Hovercraft Group</h1>
+            </div>
+            <ul class="menu">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="ball.html">Ball</a>
+                </li>
+                <li>
+                    <a href="bee.html">Bee</a>
+                </li>
+                <li>
+                    <a href="jib.html">Jib</a>
+                </li>
+                <li>
+                    <a href="phai.html">Phai</a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <nav class="navbar navbar-dark bg-dark">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </nav>
-</div>
 
-  </div>
-  
-</nav>
+    <div class="clearfix"></div>
 
-
-<header class="header">
+    <header class="header">
         <div class="container">
             <div class="header_area">
     
@@ -131,214 +51,73 @@
         </div>
     </header>
 
-     <h2>Members</h2>
-    <div class="container" style="margin-top:30px">
-  <div class="row">
-    <div class="col-sm-6">
-      
-            <div class="card" style="width:300px">
-                <img class="card-img-top" src="p11.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                <h4 class="card-title">Jiraphat Thongyoo</h4>
-                <p class="card-text">620710040<br>Data Science<br>SU76 SC48 DS2</p>
-                <a href="ball1.html" class="btn btn-primary">See Profile</a>
+    <section class="info1">
+        <div class="container">
+            <div class="info1_area">
+                <img src="p11.jpg" alt="">
+                <div class="info1_text">
+                    <h1>Jiraphat Thongyoo</h1>
+                    <p>620710040<br>Data Science<br>SU76 SC48 DS2</p>
+                    <a href="ball.html"><button>...Click...</button><br></a>
                 </div>
-            </div>
-      
-            <div class="card" style="width:300px">
-                <img class="card-img-top" src="p33.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                <h4 class="card-title">Apatsara Piromto</h4>
-                <p class="card-text">620710162<br>Data Science<br>SU76 SC48 DS2</p>
-                <a href="jib.html" class="btn btn-primary">See Profile</a>
-                </div>
-            </div>
-      
-    </div>
-    <div class="col-sm-6">
-      
-            <div class="card" style="width:300px">
-                <img class="card-img-top" src="p22.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                <h4 class="card-title">Phanthipha Makchiao</h4>
-                <p class="card-text">620710161<br>Data Science<br>SU76 SC48 DS2</p>
-                <a href="b.html" class="btn btn-primary">See Profile</a>
-                </div>
-            </div>
-      
-            <div class="card" style="width:300px">
-                <img class="card-img-top" src="p44.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                <h4 class="card-title">Worapon Boontawee</h4>
-                <p class="card-text">620710742<br>Data Science<br>SU76 SC48 DS2</p>
-                <a href="phaii.html" class="btn btn-primary">See Profile</a>
-                </div>
-            </div>
-      
-      
-    </div>
-  </div>
-</div>
-
-<br>
-
-<div class="container-fluid">
-  <div class="card-deck">
-    <div class="card">
-      <img class="card-img-top" src="R1.png" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">python</h5>
-        <p class="card-text">COMPUTER PROGRAMMING <br>DATA STRUCTURE AND ALGORITHM<br>DATA SCIENCE TOOLBOX<br>BIG DATA PROCESSING</p>
-        
-      </div>
-    </div>
-
-    <div class="card">
-      <img class="card-img-top" src="R2.png" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">R</h5>
-        <p class="card-text">STATISTICS FOR DATA SCIENCE<br>FOUNDATION OF DATA SCIENCE<br>DATA SCIENCE TOOLBOX<br>GETTING AND CLEANING DATA</p>
-        
-      </div>
-    </div>
-
-    <div class="card">
-      <img class="card-img-top" src="R3.png" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">SQL</h5>
-        <p class="card-text">DATABASE SYSTEMS <br>DATA SCIENCE TOOLBOX<br><br><br></p>
-        
-      </div>
-    </div>
-
-    <div class="card">
-      <img class="card-img-top" src="R4.png" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">HTML CSS JS BOOTSTRAP</h5>
-        <p class="card-text">WEB TECHNOLOGY<br><br><br><br></p>
-        
-      </div>
-    </div>
-
-
-  </div>   
-</div>
-
-<br>
-
-<div class="container-fluid p-3 my-3 bg-dark text-white">
-
-    <div id="demo" class="carousel slide" data-ride="carousel">
-
-        <!-- Indicators -->
-        <ul class="carousel-indicators">
-            <li data-target="#demo" data-slide-to="0" class="active"></li>
-            <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
-            <li data-target="#demo" data-slide-to="3"></li>
-            <li data-target="#demo" data-slide-to="4"></li>
-        </ul>
-
-        <!-- The slideshow -->
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="s1.jpg" alt="ball" width="600" height="500">
-            </div>
-            <div class="carousel-item">
-                <img src="s2.jpg" alt="bee" width="600" height="500">
-            </div>
-            <div class="carousel-item">
-                <img src="s3.jpg" alt="jib" width="600" height="500">
-            </div>
-            <div class="carousel-item">
-                <img src="s4.jpg" alt="phai" width="600" height="500">
-            </div>
-            <div class="carousel-item">
-                <img src="s5.jpg" alt="phai" width="600" height="500">
             </div>
         </div>
+    </section>
 
-        <!-- Left and right controls -->
-        <a class="carousel-control-prev" href="#demo" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#demo" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </a>
-    </div>
-</div>
-
-<br>
-<!-- Contact -->
-<div id="contact" class="Section Contact lazy-bg">
-        <div class="Contact-Content" id="contact-section">
-            <div class="Head-Row">
-                <div class="Head-Column">
-                <div class="Heading">
-                        <h2><span class="Markdown"></span></h2>
-                    </div>
+    <section class="info2">
+        <div class="container">
+            <div class="info2_area">
+                <div class="info2_text">
+                    <h1>Phanthipha Makchiao</h1>
+                    <p>620710161<br>Data Science<br>SU76 SC48 DS2</p>
+             
+                    <a href="bee.html"><button>...Click...</button><br></a>
                 </div>
+            
+                <img src="p22.jpg" alt="">
             </div>
-        </div>   
-</div>  
-        
-<div class="container-fluid" style="margin-top:30px">
-  <div class="row">
-    <div class="col-sm-4">
-      
-        <div class="Contact-Row">
-                <div class="Contact-Column-Social">
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FScienceSilpakornUniversity&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </div>
+    </section>
+
+    <section class="info3">
+        <div class="container">
+            <div class="info3_area">
+                <img src="p33.jpg" alt="">
+                <div class="info3_text">
+                    <h1>Apatsara Piromto</h1>
+                    <p>620710162<br>Data Science<br>SU76 SC48 DS2</p>
+                    <a href="jib.html"><button>...Click...</button><br></a>
                 </div>
-        </div> 
-      
-    </div>
-    <div class="col-sm-4">
-                <div class="Contact-Column-Social">
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcpsilpakorn&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-                </div> 
-      
-      
-    </div>
-    <div class="col-sm-4">
-            <div class="Contact-Column-Social">
-                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FDSSCSU%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> 
-            </div> 
-      
-    </div>
-  </div>
-</div>
+                
+            </div>
+        </div>
+    </section>
 
-<br>
-
-
-<div id = "onfoot" class="container--fluid" >
-      
-  <h5>Contact Group Us</h5>
-
-  <p>
-     <span><a data-toggle="tooltip" title="Follow on CPSU_Group" href="https://www.facebook.com/groups/CPSUTH"><i class="fab fa-facebook-square" style="color:rgb(255, 255, 255);font-size:72px"></i></a></span> 
-     <span><a data-toggle="tooltip" title="Follow on DSSCSU_Group" href="https://www.facebook.com/groups/DSSCSU"><i class="fab fa-facebook-square" style="color:rgb(102, 179, 255);font-size:72px"></i></a></span> 
-     <span><a data-toggle="tooltip" title="Follow on WebTechnology_Group" href="https://www.facebook.com/groups/2097980677004926"><i class="fab fa-facebook-square" style="color:rgb(0, 132, 255);font-size:72px"></i></a></span> 
-  </p>
-  <p class="float-right">
-        <a href="#"><img src="up.png" alt="ball" width="50" height="50"></a>
-  </p>
-<br>
-<br>
-<br>
- 
- 
+    <section class="info4">
+        <div class="container">
+            <div class="info4_area">
+                <div class="info4_text">
+                    <h1>Worapon Boontawee</h1>
+                    <p>620710742<br>Data Science<br>SU76 SC48 DS2</p>
+                    <a href="phai.html"><button>...Click...</button><br></a>
+                </div>
+                <img src="p44.jpg" alt="">
+            </div>
+           
+    </section>
     
+    <div class="clearfix"></div>
+
    
 
-<script>
-    $(document).ready(function(){
-      $('[data-toggle="tooltip"]').tooltip();   
-    });
-  </script>
 
+
+
+
+
+    <footer>
+        <p>Copyright &copy; 2020 | Hovercraft</p>
+    </footer>
+    
 </body>
-
 </html>
-
